32db1f18698c3d2b58538b1e849c6e3c0fa8fda8
Pratima Singh <gk1137@myamu.ac.in>
2022-04-20 10:16:10 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header/Header.js';
 
 function App() {
   return (
     <div className="App">
-      
+          <Header/>            
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header(){
     return(
         <div>
-            <h1>Header Page of Seller Page</h1>
+            <h1>Header of Seller Page</h1>
             </div>
     )
 }
